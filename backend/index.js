a5cec10e725fc7f804a36bd0d51086c37d2c4150
KevinSizzAL <kguzman@yourappland.com>
2021-06-30 17:40:20 -0500

    feat(): cors headers

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,17 @@ dotenv.config();
 
 app.use(express.json())
 app.use(cors([{origin:'*', credentials:true}]));
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-})
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
+});
+
 app.set('port', process.env.PORT);
 
 server.listen(process.env.PORT, () => {
